1be63d915a0ffc9bffecaa7a1fe888a3c3ce9546
David Weir <david.weir4@mail.dcu.ie>
2022-01-11 20:18:24 +0000

    Live web feed face detection script

---
+++ src/webcamFaceDetection.py
@@ -0,0 +1,40 @@
+from cv2 import cv2
+import sys
+
+cascade = sys.argv[1]
+
+# haar cascade - create a cascade initialized with the face cascade
+# Loads the face cascade (XML file containing data to detect faces) into memory
+faceCascade = cv2.CascadeClassifier(cascade)
+
+webcam = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # set video source as default webcam
+
+# FACE DETECTION
+
+while True:
+
+    ret, frame = webcam.read()  # capture webcam feed frame-by-frame
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,  # greyscale image
+        scaleFactor=1.5,  # compensates for the different distances from the camera of the faces
+        minNeighbors=5,  # defines how many objects are detected near the current object before
+                         # declaring a face is detected
+        minSize=(30, 30),  # size of each window
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
+    # draw a identifying rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    cv2.imshow("Faces Found", frame)
+
+    # if q is pressed -> exit script
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+webcam.release()
+cv2.destroyAllWindows()
