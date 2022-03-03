3147f88762a3816b43554fe780f96e675e4c1a29
David Weir <david.weir4@mail.dcu.ie>
2022-03-03 21:21:39 +0000

    Created extract function for importing in gui.py

--- src/feature_extract.py
+++ src/feature_extract.py
@@ -4,38 +4,39 @@
 import os, cv2, pickle, face_recognition
 from imutils import paths
 
-# retrieve the path of each sub-data folder in the datasets folder
-# the datasets folder contains sub-folders containing images of various people
-imgPaths = list(paths.list_images('datasets'))
+def extract():
+    # retrieve the path of each sub-data folder in the datasets folder
+    # the datasets folder contains sub-folders containing images of various people
+    imgPaths = list(paths.list_images('datasets'))
 
-names, knownEncodings = [], []
+    names, knownEncodings = [], []
 
-# loop through the image paths list
-for (i, imgPath) in enumerate(imgPaths):
-    name = imgPath.split(os.path.sep)[-2]  # extract person's name from the image path
+    # loop through the image paths list
+    for (i, imgPath) in enumerate(imgPaths):
+        name = imgPath.split(os.path.sep)[-2]  # extract person's name from the image path
 
-    # load the input image: convert it from OpenCV ordering BGR to dlib ordering RGB
-    image = cv2.imread(imgPath)
-    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # load the input image: convert it from OpenCV ordering BGR to dlib ordering RGB
+        image = cv2.imread(imgPath)
+        rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    # locate faces using the face_recognition library
-    boxes = face_recognition.face_locations(rgb, model='hog')
+        # locate faces using the face_recognition library
+        boxes = face_recognition.face_locations(rgb, model='hog')
 
-    # compute facial embeddings of the face
-    encodings = face_recognition.face_encodings(rgb, boxes)
+        # compute facial embeddings of the face
+        encodings = face_recognition.face_encodings(rgb, boxes)
 
-    # loop over encodings list
-    for encoding in encodings:
-        knownEncodings.append(encoding)
-        names.append(name)
+        # loop over encodings list
+        for encoding in encodings:
+            knownEncodings.append(encoding)
+            names.append(name)
 
-# save encodings and name in a data dictionary
-data = {
-    "encodings": knownEncodings,
-    "names": names
-}
+    # save encodings and name in a data dictionary
+    data = {
+        "encodings": knownEncodings,
+        "names": names
+    }
 
-# save the data into a file for use later
-f = open("face_enc", "wb")
-f.write(pickle.dumps(data))
-f.close()
+    # save the data into a file for use later
+    f = open("face_enc", "wb")
+    f.write(pickle.dumps(data))
+    f.close()
